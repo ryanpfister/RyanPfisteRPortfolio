477a65c87871b297abf9e6a3f5d6f4e089d15a1f
Ryan Pfister <57775823+ryanpfister@users.noreply.github.com>
2023-10-03 13:52:06 -0400

    Update index.js

--- src/pages/NotFound/index.js
+++ src/pages/NotFound/index.js
@@ -1,11 +1,15 @@
-import { Link } from "react-router-dom";
+import React from "react";
+import "./style.css";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import { Container, Row, Col } from "react-bootstrap";
+import { dataportfolio, meta } from "../../content_option";
 
-export default function NotFound() {
-    return (
-        <div>
-            <h1>Oops! You seem to be lost.</h1>
-            <p>Here are some helpful links:</p>
-            <Link to='/'>Home</Link>
-        </div>
-    )
-}
+export const NotFound = () => {
+  return (
+    <div>
+    <h1>Oops! You seem to be lost.</h1>
+    <p>Here are some helpful links:</p>
+    <Link to='/'>Home</Link>
+</div>
+  );
+};
