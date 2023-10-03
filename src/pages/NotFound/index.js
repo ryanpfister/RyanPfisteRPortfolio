69a850697ee309eedbe8cfe93e0e2a1427d15080
Ryan Pfister <57775823+ryanpfister@users.noreply.github.com>
2023-10-03 13:52:42 -0400

    Update index.js

--- src/pages/NotFound/index.js
+++ src/pages/NotFound/index.js
@@ -8,8 +8,6 @@ export const NotFound = () => {
   return (
     <div>
     <h1>Oops! You seem to be lost.</h1>
-    <p>Here are some helpful links:</p>
-    <Link to='/'>Home</Link>
 </div>
   );
 };
